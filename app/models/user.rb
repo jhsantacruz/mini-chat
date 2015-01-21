35ad81a5cba0b6664ead05f279a964ca7ebf1cff
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-20 23:32:43 -0800

    Added class method to User model to handle the authorization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id          :integer          not null, primary key
+#  name        :string(255)
+#  avatar_url  :string(255)
+#  provider    :string(255)
+#  profile_url :string(255)
+#  uid         :string(255)
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class User < ActiveRecord::Base
+	has_many :comments, dependent: :delete_all
+
+	def self.from_omniauth(auth)
+    provider = auth.provider
+    uid = auth.uid
+    info = auth.info.symbolize_keys!
+    user = User.find_or_initialize_by(uid: uid, provider: provider)
+    user.name = info.name
+    user.avatar_url = info.image
+    user.profile_url = info.urls.send(provider.capitalize.to_sym)
+    user.save!
+    user
+  end
 end
