484e486fb33a1e293f4cc9d37322fc4c089236bd
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-20 23:27:34 -0800

    Configured the omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
   #          scope: 'public_profile', display: 'page', image_size: 'square'
 
-  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], image_size: 'normal'
+  provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret, image_size: 'normal'
 end
