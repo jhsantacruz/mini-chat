a8e9de2051b3bf162c7f3036443ba33f2e99079f
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-21 22:57:28 -0800

    added Route for comments.

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/auth/failure', to: 'sessions#auth_fail'
   get '/sign_out', to: 'sessions#destroy', as: :sign_out
+
+  resources :comments, only: [:new, :create]
+  root to: 'comments#new'
 end
