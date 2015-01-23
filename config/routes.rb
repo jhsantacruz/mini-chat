25573ea9c9dbc1ced949932b16b10a7413965149
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-22 23:50:48 -0800

    Polling method to update the comment conversation

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/auth/failure', to: 'sessions#auth_fail'
   get '/sign_out', to: 'sessions#destroy', as: :sign_out
 
-  resources :comments, only: [:new, :create]
+  resources :comments, only: [:new, :create, :index]
   root to: 'comments#new'
 end
