3dc7ffcde3238ea381328f5e3e32be95cab4399e
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-22 23:21:14 -0800

    Fixed problem with session controller.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ActiveRecord::Base
 	validates :body, presence: true, length: {maximum: 2000}
   belongs_to :user
+
+  def self.remove_excessive!
+  	if all.count > 100
+      order('created_at ASC').limit(all.count - 50).destroy_all
+    end
+  end
 end
