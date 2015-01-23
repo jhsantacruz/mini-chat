b5f9cece4d8b0228c9bdfb76fa91926834fa80ff
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-22 22:43:06 -0800

    Create method function in the controller, added validation in the body length.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,18 @@ class CommentsController < ApplicationController
   end
  
   def create
-  end
+	  if current_user
+	    @comment = current_user.comments.build(comment_params)
+	    if @comment.save
+	      flash[:success] = 'Your comment was successfully posted!'
+	    else
+	      flash[:error] = 'Your comment cannot be saved.'
+	    end
+	  end
+	  redirect_to root_url
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:body)
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
+	validates :body, presence: true, length: {maximum: 2000}
   belongs_to :user
 end
