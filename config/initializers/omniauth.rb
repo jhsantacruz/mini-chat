1a65a047917832add2c01eaaa26c5664c9bf0c49
Juan Santa Cruz <jhsantacruz@icloud.com>
2015-01-24 23:09:17 -0800

    Starting to work on the Facebook omniAuth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  #          scope: 'public_profile', display: 'page', image_size: 'square'
+  provider :facebook, Rails.application.secrets.facebook_key, Rails.application.secrets.twitter_secret,
+           scope: 'public_profile', display: 'page', image_size: 'square'
 
   provider :twitter, Rails.application.secrets.twitter_key, Rails.application.secrets.twitter_secret, image_size: 'normal'
 end
